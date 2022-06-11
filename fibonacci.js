2dae3ca974f439acc62ff1626e0091cb02ccc892
abbadmus <35636750+abbadmus@users.noreply.github.com>
2022-06-11 18:19:19 +0100

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+# conventional method
+const fib1 = (n) => {
+  if (n <= 2) return 1;
+  return fib1(n - 1) + fib1(n - 2);
+};
+
+console.log(fib1(50));
+
+
+console.log(a(10));
+
+const fib2 = (n, meno = {}) => {
+  if (n in meno) return meno[n];
+  if (n <= 2) return 1;
+  meno[n] = fib2(n - 1, meno) + fib2(n - 2, meno);
+  return meno[n];
+};
+
+console.log(fib1(60));
